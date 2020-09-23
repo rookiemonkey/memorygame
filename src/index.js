09d48d8cce9446e35700903c62869cddb4951e4f
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-23 17:53:06 +0800

    fixed some methods

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Memorygame from './app';
-import './index.scss'; // global css
+import './assets/scss/index.scss'; // global css
 
 ReactDOM.render(
   <React.StrictMode>
