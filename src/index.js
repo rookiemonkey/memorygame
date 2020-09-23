c78262fd0949cf4e2265aa6d76ae46523a6a6342
rookiemonkey <kevinroirigorbasina@pm.me>
2020-09-23 18:06:30 +0800

    fixed some methods

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactHowler from 'react-howler'
 import Memorygame from './app';
 import './assets/scss/index.scss'; // global css
 
 ReactDOM.render(
   <React.StrictMode>
+    <ReactHowler
+      src={require('./assets/audios/background.mp3')}
+      playing={true}
+      loop={true}
+    />
     <Memorygame />
   </React.StrictMode>,
   document.getElementById('root')
